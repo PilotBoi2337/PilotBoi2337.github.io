a29f838bed55aaedd2abcdcb65d53426c766d3d8
Jared <jared.jl.lim@gmail.com>
2024-02-11 19:26:10 -0800

    testing react-qr-reader

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import QRModal from './QRModal';
 import blueField from './assets/blueField.png';
 import redField from './assets/redField.png';
 import logo from './assets/aembot_icon_vector.svg';
+import {QrReader} from 'react-qr-reader';
 
 function App() {
 
@@ -11,6 +12,25 @@ function App() {
   const [showQR, setShowQR] = useState(false);
   const [finalData, setQrData] = useState(''); // Initialize qrData as a state variable
   
+  //for pageQR (the page with the qr scanner)
+  const [showScanner, setShowScanner] = useState(false);
+  const [scannedData, setData] = useState('No result');
+
+  const handleScan = scannedData => {
+    if (scannedData) {
+      setData(scannedData);
+    }
+  }
+
+  const handleError = err => {
+    console.error(err);
+  }
+
+  const handleStartScan = () => {
+    setShowScanner(true);
+  }
+
+
   //placeholders
   const qrTitle = "Here it is!";
   var qrData = "";
@@ -34,7 +54,7 @@ function App() {
   function saveAsCSV(array){
     var data;
     for(var index in array){
-      data += array[index] + ", ";
+      data += array[index] + ",";
     };
     while(data.substring(0, 9) == "undefined"){
       data = data.substring(9);
@@ -137,6 +157,11 @@ function App() {
     if(qrData1.substring(0, 9) == "undefined"){
       qrData1 = qrData1.substring(9);
     }
+
+    //assuming preload by default
+    logNote("preload");
+    timeToPickup = 0;
+
     qrData = qrData1 + qrData2 + qrData3 + qrData4;
     console.log(qrData);
     //unhide next page
@@ -228,7 +253,7 @@ function App() {
     //log starting position
     startPos = startingPos;
     document.getElementById(startPos).classList.add('startPosSelected');
-    qrData2 = startTime + startPos + autoNoteData;
+    qrData2 = startTime + startPos;
   }
 
   function mobilityToggle(){
@@ -257,12 +282,15 @@ function App() {
     document.getElementById('t5').classList.remove('noteSelected');
 
     pickupTime = new Date();
-    if(note){
+    if(note && note != "preload"){
       //auto
       document.getElementById('mobilityButton').classList.add('selected');
       mobility = "yes";
       heldNote = note;
       document.getElementById(heldNote).classList.add('noteSelected');
+    }
+    else if (note == "preload"){
+      heldNote = note;
     }
     timeToPickup = (pickupTime - startTime) / 1000;
 
@@ -271,59 +299,70 @@ function App() {
     document.getElementById('ampB').classList.remove('disabled');
     document.getElementById('droppedB').classList.remove('disabled');
     document.getElementById('trapB').classList.remove('disabled');
-  }
 
-  function droppedNote() {
-    if(document.getElementById('pickUpB').classList.contains('disabled')){
-      const scoringLocation = "dropped";
-      const endTime = new Date();
-      const timeToScore = (endTime - pickupTime) / 1000;
-      
-      if(heldNote) {
-        //auto
-        autoNoteData += saveAsCSV([heldNote, scoringLocation, timeToPickup, timeToScore]);
-        if(autoNoteData.substring(0, 9) == "undefined"){
-          autoNoteData = autoNoteData.substring(9);
-        };
-        document.getElementById(heldNote).classList.add('hidden');
-      } else {
-        //teleOp
-        teleOpNoteData += saveAsCSV([scoringLocation, timeToPickup, timeToScore]);
-        if(teleOpNoteData.substring(0, 9) == "undefined"){
-          teleOpNoteData = teleOpNoteData.substring(9);
-        };
-      }
-    }
-    //reseting values for next cycle
-    heldNote = null;
-    pickupTime = null;
-
-    document.getElementById('pickUpB').classList.remove('disabled');
-    document.getElementById('speakerB').classList.add('disabled');
-    document.getElementById('ampB').classList.add('disabled');
-    document.getElementById('droppedB').classList.add('disabled');
-    document.getElementById('trapB').classList.add('disabled');
+    document.getElementById("speakerB").classList.remove('lastSelected');
+    document.getElementById("ampB").classList.remove('lastSelected');
+    document.getElementById("droppedB").classList.remove('lastSelected');
+    document.getElementById("trapB").classList.remove('lastSelected');
   }
 
   function scoredNote(place) {
-    if(document.getElementById('pickUpB').classList.contains('disabled')){
+    if(heldNote == "preload" && !document.getElementById('page3').classList.contains('hidden')) {
+      //if preload is brought to teleOp
+      const scoringLocation = place;
+      const endTime = new Date();
+      const timeToScore = (endTime - startTime) / 1000;
+      console.log("preloaded piece scored in teleOp");
+      teleOpNoteData += saveAsCSV([scoringLocation, timeToPickup, timeToScore]);
+      if(teleOpNoteData.substring(0, 9) == "undefined"){
+        teleOpNoteData = teleOpNoteData.substring(9);
+      };
+
+      //reseting values for next cycle
+      heldNote = null;
+      pickupTime = null;
+
+      document.getElementById('pickUpB').classList.remove('disabled');
+      document.getElementById('speakerB').classList.add('disabled');
+      document.getElementById('ampB').classList.add('disabled');
+      document.getElementById('droppedB').classList.add('disabled');
+      document.getElementById('trapB').classList.add('disabled');
+    }
+    if(document.getElementById('pickUpB').classList.contains('disabled')) {
+      //piece picked up is scored
       const scoringLocation = place;
       const endTime = new Date();
       const timeToScore = (endTime - pickupTime) / 1000;
-      if(heldNote){
+      console.log("heldNote: " + heldNote);
+      console.log(!document.getElementById('page3').classList.contains('hidden'));
+      if(heldNote && !document.getElementById('page2').classList.contains('hidden')){
         //auto
         autoNoteData += saveAsCSV([heldNote, scoringLocation, timeToPickup, timeToScore]);
         if(autoNoteData.substring(0, 9) == "undefined"){
           autoNoteData = autoNoteData.substring(9);
         };
         console.log(autoNoteData);
-        document.getElementById(heldNote).classList.add('hidden');
-      } else {
+        if(heldNote != "preload"){document.getElementById(heldNote).classList.add('hidden');}
+        
+      }
+      else if(heldNote == "preload" && !document.getElementById('page3').classList.contains('hidden')) {
+        console.log("preloaded piece scored in teleOp");
+        teleOpNoteData += saveAsCSV([scoringLocation, timeToPickup, timeToScore]);
+        if(teleOpNoteData.substring(0, 9) == "undefined"){
+          teleOpNoteData = teleOpNoteData.substring(9);
+        };
+      }
+      else {
+        document.getElementById("speakerB").classList.remove('lastSelected');
+        document.getElementById("ampB").classList.remove('lastSelected');
+        document.getElementById("droppedB").classList.remove('lastSelected');
+        document.getElementById("trapB").classList.remove('lastSelected');
         //teleOp
         teleOpNoteData += saveAsCSV([scoringLocation, timeToPickup, timeToScore]);
         if(teleOpNoteData.substring(0, 9) == "undefined"){
           teleOpNoteData = teleOpNoteData.substring(9);
         };
+        document.getElementById(place + "B").classList.add('lastSelected');
       }
       
       //reseting values for next cycle
@@ -338,34 +377,22 @@ function App() {
     }
   }
 
-  function scoredTrap() {
-    if(document.getElementById('pickUpB').classList.contains('disabled')){
-      const scoringLocation = "trap";
-      const endTime = new Date();
-      const timeToScore = (endTime - pickupTime) / 1000;
-      teleOpNoteData += saveAsCSV([scoringLocation, timeToPickup, timeToScore]);
-      if(teleOpNoteData.substring(0, 9) == "undefined"){
-        teleOpNoteData = teleOpNoteData.substring(9);
-      };
-    }
-
-    heldNote = null;
-    pickupTime = null;
-
-    document.getElementById('pickUpB').classList.remove('disabled');
-    document.getElementById('speakerB').classList.add('disabled');
-    document.getElementById('ampB').classList.add('disabled');
-    document.getElementById('droppedB').classList.add('disabled');
-    document.getElementById('trapB').classList.add('disabled');
-  }
-
   function climb() {
-    var climbStart = new Date();
-    climbStartTime = (climbStart - startTime)/1000;
+    if(!document.getElementById('climbB').classList.contains('selected')) {
+      document.getElementById('climbB').classList.add('selected');
+      var climbStart = new Date();
+      climbStartTime = (climbStart - startTime)/1000;
+    } else {
+      //deselecting climb
+      climbStartTime = 'no climb';
+      document.getElementById('climbB').classList.remove('selected');
+    }
+    
+
   }
 
   function nextPage2() {
-    qrData2 = saveAsCSV([startPos, autoNoteData]);
+    qrData2 = saveAsCSV([startPos]);
     console.log(qrData2);
     qrData = qrData1 + qrData2 + qrData3 + qrData4;
     console.log(qrData);
@@ -373,9 +400,16 @@ function App() {
     document.getElementById('page2').classList.add('hidden');
 
     //if a note is brought to teleOp:
-    if(heldNote){
+    if(heldNote && heldNote !="preload"){
       heldNote == null;
       document.getElementById('pickUpB').classList.add('disabled');
+      document.getElementById('speakerB').classList.remove('disabled');
+      document.getElementById('ampB').classList.remove('disabled');
+      document.getElementById('droppedB').classList.remove('disabled');
+      document.getElementById('trapB').classList.remove('disabled');
+    }
+    else if (heldNote == "preload"){
+      document.getElementById('pickUpB').classList.remove('disabled');
       document.getElementById('speakerB').classList.remove('disabled');
       document.getElementById('ampB').classList.remove('disabled');
       document.getElementById('droppedB').classList.remove('disabled');
@@ -442,7 +476,7 @@ function App() {
 
   function nextPage3() {
     if(climbStartTime == undefined){climbStartTime = "noClimb";}
-    qrData3 = saveAsCSV(["teleOp", teleOpNoteData]) + saveAsCSV(["endgame", trapCount, climbStartTime]);
+    qrData3 = saveAsCSV(["endgame", climbStartTime]);
     console.log(qrData3);
     qrData = qrData1 + qrData2 + qrData3 + qrData4;
     console.log(qrData);
@@ -468,20 +502,27 @@ function App() {
     //compile fails list
     var fails = document.querySelector('fieldSet');
     var failsSelected = Array.from(fails.querySelectorAll('input[type="checkbox"]:checked')).map(input => input.value);
-    var failsList;
+    var failsList = "";
     for(var i in failsSelected) {
-      failsList += failsSelected[i] + " | ";
+      failsList += failsSelected[i] + ", ";
     };
     if(failsList == "undefined"){
       failsList == "no fails";
     };
 
-    qrData4 = saveAsCSV([pickupMethod, climbStatus]) + saveAsCSV([failsList]) + saveAsCSV([otherInfo])
-    console.log(qrData4);
+    qrData4 = saveAsCSV([pickupMethod, climbStatus]) + saveAsCSV(["\"" + failsList + "\""]) + saveAsCSV(["\"" + otherInfo + "\""])
     qrData = qrData1 + qrData2 + qrData3 + qrData4;
+    console.log(qrData);
+    qrData += saveAsCSV(["auto"]) + saveAsCSV([autoNoteData]) + saveAsCSV(["teleOp"]) + saveAsCSV([teleOpNoteData]);
+    
     document.getElementById('page5').classList.remove('hidden');
     document.getElementById('page4').classList.add('hidden');
-    setQrData(qrData);
+
+    const csvBlob = new Blob([qrData], { type: 'text/csv' });
+    const fileUrl = URL.createObjectURL(csvBlob);
+    //actually encodes the data into the QRCode
+    //if link based download fails, replace fileUrl with qrData
+    setQrData(fileUrl);
     console.log(qrData);
 
   }
@@ -530,6 +571,18 @@ function App() {
         <button class="bButton hidden" id="nextPage1Button" onClick={() => nextPage1()}>NEXT PAGE</button>
       </div>
 
+      <div class="page pageQR" id="pageQR">
+      <button onClick={handleStartScan}>Start Scanning</button>
+      {showScanner && (
+          <QrReader
+            delay={300}
+            onError={handleError}
+            onResult={handleScan}
+            style={{ width: '100%' }}
+        />)}
+        <p>{scannedData}</p>
+      </div>
+
       <div class="page hidden page2" id="page2">
         {/* auto (map) mobilityInAuto automatically checks if game piece is pressed*/}
         <div class="topButtons">
@@ -555,7 +608,7 @@ function App() {
         </div>
         <button class="scored" id="speakerButton" onClick={() => scoredNote("speaker")}>Scored in Speaker</button>
         <button class="scored" id="ampButton" onClick={() => scoredNote("amp")}>Scored in Amp</button>
-        <button class="scored" id="droppedButton" onClick={() => droppedNote()}>Dropped</button>
+        <button class="scored" id="droppedButton" onClick={() => scoredNote("dropped")}>Dropped</button>
         <button class="scored" id="mobilityButton" onClick={() => mobilityToggle()}>Mobility</button>
       </div>
 
@@ -570,10 +623,10 @@ function App() {
         <button class="scored" id="pickUpB" onClick={() => logNote()}>Pickup</button>
         <button class="scored disabled" id="speakerB" onClick={() => scoredNote("speaker")}>Scored in speaker</button>
         <button class="scored disabled" id="ampB" onClick={() => scoredNote("amp")}>Scored in Amp</button>
-        <button class="scored disabled" id="droppedB" onClick={() => droppedNote()}>Dropped</button>
-        <button class="scored disabled" id="trapB" onClick={() => scoredTrap()}>Trap</button>
+        <button class="scored disabled" id="droppedB" onClick={() => scoredNote("dropped")}>Dropped</button>
+        <button class="scored disabled" id="trapB" onClick={() => scoredNote("trap")}>Trap</button>
 
-        <button class="scored" onClick={() => climb()}>Climb</button>
+        <button class="scored" id="climbB" onClick={() => climb()}>Climb</button>
         
       </div>
 
@@ -589,10 +642,10 @@ function App() {
           <div class="question">
             <label for="pickUpMethods">PICKUP METHOD</label>
             <select name="pickUpMethods" id="pickUpMethods" required>
-                <option value="ground and source" selected>Ground and Source</option>
-                <option value="ground only">Ground only</option>
-                <option value="source only">Source only</option>
-                <option value="none">none</option>
+                <option value="none" selected>None</option>
+                <option value="ground only">Ground Only</option>
+                <option value="source only">Source Only</option>
+                <option value="ground and source">Ground and Source</option>
             </select>
           </div>
           <p></p>
@@ -600,8 +653,8 @@ function App() {
             <label for="climbStatus">CLIMB </label>
             <select name="climbStatus" id="climbStatus" required>
                 <option value="no attempt">No Attempt</option>
-                <option value="climb success">success</option>
-                <option value="climb failed">failed</option>
+                <option value="climb success">Success</option>
+                <option value="climb failed">Failed</option>
             </select>
           </div>
 
@@ -641,6 +694,10 @@ function App() {
             <div class="fail">
               <label for="fail8">Got Stuck</label>
               <input type="checkbox" id="fail8" name="fail8" value="got stuck"></input>
+            </div>
+            <div class="fail">
+              <label for="fail9">Didn't Field Robot</label>
+              <input type="checkbox" id="fail9" name="fail9" value="didn't field robot"></input>
             </div>
           </fieldset>
         </div>
